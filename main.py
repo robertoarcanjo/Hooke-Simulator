c81ccc83cb37c0537f1fb2e99e3b7398a990cfc3
robertoarcanjo <jose.arcanjo@ee.ufcg.edu.br>
2022-12-19 09:59:16 -0300

    #4

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import sqlite3 
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from ttkwidgets import Table
 import math
 
 root = tk.Tk()
@@ -11,14 +12,37 @@ root.title("ICM - Tensão vs Deformação")
 root.geometry("750x700")
 
 label = tk.Label(root, text = "Seja bem vindo!")
-
+label2 = tk.Label(root, text = "Determine um ângulo para obter um gráfico tensão deformação na zona elástica, em função deste ângulo")
+label2.pack()
 entry = tk.Entry(root)
 entry.pack()
+texto_digitado = entry.get()
+angulo = 60
 
 def processar_texto():
     texto_digitado = entry.get()
+    angulo = int(texto_digitado)
     entry.delete(0, "end")
-    resultado.config(text=texto_digitado)
+    resultado.config(text="O ângulo é " + texto_digitado + "°")
+    update_plot(angulo)
+
+def update_plot(angulo):
+    
+    alfa = math.tan(math.radians(angulo))
+
+    x = [0.01 * i for i in range(100)]
+    y = [alfa * i for i in x]
+
+    ax.clear()
+    ax.plot(x, y)
+
+    # Calcular o ponto médio
+    x_mid = (x[0] + x[-1]) / 2
+    y_mid = (y[0] + y[-1]) / 2
+
+    # Adicionar o ângulo e o ponto médio ao gráfico
+    ax.text(0.05, 0.95, f"ângulo: {angulo}°", transform=ax.transAxes, verticalalignment="top")
+    canvas.draw()
 
 button = tk.Button(root, text="Enviar", command=processar_texto)
 button.pack()
@@ -29,23 +53,14 @@ label.pack()
 
 resultado.pack()
 
-
-x = [1, 2, 3, 4]
-y = [1, 4, 9, 16]
-
 fig = plt.figure()
-
 ax = fig.add_subplot(111)
 ax.set_xlabel("Deformação")
 ax.set_ylabel("Tensão ")
 
-
-ax.plot(x, y)
-
 canvas = FigureCanvasTkAgg(fig, master=root)
-
 plot_widget = canvas.get_tk_widget()
-
 plot_widget.pack(side="top", fill="both", expand=True)
+
 
 root.mainloop()
